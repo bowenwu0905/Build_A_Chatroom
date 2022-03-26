faa456e454779e65a486b6e8ec195bf4fe4d2dca
xcjiang <jiang.xiaoc@northeastern.edu>
2022-03-25 22:16:56 -0700

    update UI

--- assignment4/src/main/java/assignment4/Main.java
+++ assignment4/src/main/java/assignment4/Main.java
@@ -7,11 +7,11 @@ package assignment4;
  */
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     run();
   }
 
-  public static void run() {
+  public static void run() throws Exception {
     UserInterface userInterface = new UserInterface();
     userInterface.display();
   }
