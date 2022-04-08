1bf64c7b87b90bcdeeb406be42f6f3491b217a4a
bowenwu0826 <68851709+bowenwu0826@users.noreply.github.com>
2022-04-08 15:21:36 -0700

    test

--- assignment5/src/test/java/sequentialSolution/GeneratorTest.java
+++ assignment5/src/test/java/sequentialSolution/GeneratorTest.java
@@ -2,17 +2,25 @@ package sequentialSolution;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GeneratorTest {
+  Generator generator;
+  String rootPath = new File("").getAbsolutePath();
 
   @BeforeEach
   void setUp() {
+    generator = new Generator();
   }
 
   @Test
   void absolutePathChange() {
+    String path = "";
+    String x = generator.absolutePathChange(path);
+    Assertions.assertEquals(rootPath+"/",x);
   }
 
   @Test
