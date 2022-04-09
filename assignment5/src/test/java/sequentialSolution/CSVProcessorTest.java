1432775568c200fdf1a89740728779dd220593fe
bowenwu0826 <68851709+bowenwu0826@users.noreply.github.com>
2022-04-09 15:40:12 -0700

    test

--- assignment5/src/test/java/sequentialSolution/CSVProcessorTest.java
+++ assignment5/src/test/java/sequentialSolution/CSVProcessorTest.java
@@ -2,9 +2,14 @@ package sequentialSolution;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.opencsv.exceptions.CsvValidationException;
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,13 +19,12 @@ class CSVProcessorTest {
   CSVProcessor c1;
   Map<String, Map<String, Integer>> res;
   String rootPath = new File("").getAbsolutePath();
-  String fileAddress;
 
   @BeforeEach
   void setUp() {
+    inputArgs = new String[1];
     c1 = new CSVProcessor(inputArgs);
-    fileAddress = c1.absolutePathChange("src/test/java/sequentialSolution/testCourses.csv");
-    inputArgs
+    inputArgs[0] = "src/test/java/sequentialSolution";
   }
 
   @Test
@@ -46,16 +50,24 @@ class CSVProcessorTest {
   }
 
   @Test
-  void process() {
+  void process() throws CsvValidationException, IOException {
+    c1.process();
+    Assertions.assertEquals("{AAA_2013J={10=1}, BBB_2013J={10=1}, AAA_2014J={10=1}}", c1.getRes().toString());
   }
 
   @Test
-  void readCourse() {
+  void readCourse() throws CsvValidationException, IOException {
+    c1.readCourse();
+    Set<String> expected = new HashSet<>(Arrays.asList("AAA_2013J", "AAA_2014J","BBB_2013J"));
+    Assertions.assertEquals(expected, c1.getRes().keySet());
 
   }
 
   @Test
-  void countCourse() {
+  void countCourse() throws CsvValidationException, IOException {
+    c1.readCourse();
+    c1.countCourse();
+    Assertions.assertEquals("[{10=1}, {10=1}, {10=1}]", c1.getRes().values().toString());
 
   }
 
@@ -73,6 +85,6 @@ class CSVProcessorTest {
 
   @Test
   void testToString() {
-    Assertions.assertEquals("CSVProcessor{inputArgs=null, res={}}", c1.toString());
+    Assertions.assertEquals("CSVProcessor{inputArgs=[src/test/java/sequentialSolution], res={}}", c1.toString());
   }
 }
