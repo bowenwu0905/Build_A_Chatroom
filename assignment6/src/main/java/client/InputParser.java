0af53cc1d6b6191b7489f5456b7ce433e5f3f5ad
zitao <Ofcourseistillloveyou>
2022-04-20 18:09:09 -0700

    first

---
+++ assignment6/src/main/java/client/InputParser.java
@@ -0,0 +1,19 @@
+package client;
+
+public class InputParser {
+  public InputParser(){
+
+  }
+
+  public String inputParse(String Input){
+    return "";
+  }
+
+  public boolean checkInput(String Input){
+    return true;
+
+  }
+
+
+
+}
--- assignment6/src/main/java/protocol/ProtocolImp.java
+++ assignment6/src/main/java/protocol/ProtocolImp.java
@@ -1,5 +1,8 @@
 package protocol;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * implement of State
  *
@@ -17,6 +20,21 @@ public enum ProtocolImp implements Protocol {
   FAILED_MESSAGE,
   SEND_INSULT;
 
+  public final static Map<ProtocolImp,Integer> messageToIdr= new HashMap<>(){{
+    put(ProtocolImp.CONNECT_MESSAGE,19);
+    put(ProtocolImp.CONNECT_RESPONSE,20);
+    put(ProtocolImp.DISCONNECT_MESSAGE,21);
+    put(ProtocolImp.QUERY_USERS,22);
+    put(ProtocolImp.QUERY_RESPONSE,23);
+    put(ProtocolImp.BROADCAST_MESSAGE,24);
+    put(ProtocolImp.DIRECT_MESSAGE,25);
+    put(ProtocolImp.FAILED_MESSAGE,26);
+    put(ProtocolImp.SEND_INSULT,27);
+  }};
+
+//TODO: 1. add messange to ID and ID to messange map
+//  2. encoding can be break down to piece it will reaturn a list od string: connectMessange(type,Int,Sting)->list<string>
+// 3.
   @Override
   public String encode(String message) {
     ProtocolImp messageType = getMessageType(message);
