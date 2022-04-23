db911530980667ca8a120eeca7d8fb75395044fb
xcjiang <jiang.xiaoc@northeastern.edu>
2022-04-23 14:59:11 -0700

    a6 server code

--- assignment6/src/main/java/server/ServerHandler.java
+++ assignment6/src/main/java/server/ServerHandler.java
@@ -3,12 +3,16 @@ package server;
 import assignment4.Grammar;
 import assignment4.JsonReader;
 import com.sun.source.tree.Scope;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Semaphore;
 import protocol.MessageType;
@@ -27,6 +31,7 @@ public class ServerHandler implements Runnable {
   private ConcurrentHashMap<String, Socket> socketMap;
   private Grammar grammar;
   private JsonReader jsonReader;
+  private byte[] buffer;
 
   public ServerHandler(Semaphore semaphore, ServerSocket serverSocket) {
     this.semaphore = semaphore;
@@ -42,46 +47,93 @@ public class ServerHandler implements Runnable {
       try {
         this.semaphore.acquire();
         Socket socket = this.serverSocket.accept();
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        String line;
-        // !line.equals(logoff)
-        line = in.readLine();
-        MessageType type = protocol.getMessageType(line);
-        switch (type) {
-          case BROADCAST_MESSAGE -> {
-          } case SEND_INSULT -> {
-            String insultMessage = grammar.textGenerator("start", jsonReader.jsonProcess());
-
-          } case CONNECT_MESSAGE -> {
-
-            socketMap.put(protocol.decode(type, line), socket);
-            // success
-            String reponse = "" + socketMap.size();
-            // fail
-
-
-            out.write(protocol.encode(MessageType.CONNECT_RESPONSE, reponse));
-          } case QUERY_USERS -> {
-            String username = protocol.decode(type, line);
-            Socket socket1 = socketMap.get(username);
-
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            out.write(protocol.encode(MessageType.QUERY_RESPONSE, username));
-          } case DISCONNECT_MESSAGE -> {
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        int identifier = in.readInt();
+        switch (ProtocolImp.idrToMessage.get(identifier)) {
+          case CONNECT_MESSAGE -> {
+            int size = in.readInt();
+            buffer = new byte[size];
+            in.read(buffer, 0, size);
+            String username = new String(buffer);
+            boolean status = false;
+            String response = null;
+            if (!socketMap.containsKey(username)) {
+              socketMap.put(username, socket);
+              // success
+              response = "There are " + socketMap.size() + " other connected clients";
+              status = true;
+            } else {
+              // fail
+              response = "Already have established the connection";
+            }
+            protocol.encode(MessageType.CONNECT_RESPONSE, List.of(String.valueOf(status), response));
+            // todo
+            out.write(protocol.encode(MessageType.CONNECT_RESPONSE, response));
+          }
+          case DISCONNECT_MESSAGE -> {
             String username = protocol.decode(type, line);
             Socket socket1 = socketMap.get(username);
             socket1.close();
             socketMap.remove(username);
           }
+          case BROADCAST_MESSAGE -> {
+          }
+          case SEND_INSULT -> {
+            String insultMessage = grammar.textGenerator("start", jsonReader.jsonProcess());
+
+          }
+          case QUERY_USERS -> {
+            String username = protocol.decode(type, line);
+            Socket socket1 = socketMap.get(username);
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.write(protocol.encode(MessageType.QUERY_RESPONSE, username));
+          }
+
         }
 
-        if (socketMap.size() == 0)
-          break;
+        while (true) {
 
-        in.close();
+          MessageType type = protocol.getMessageType(line);
+          switch (type) {
+            case BROADCAST_MESSAGE -> {
+            }
+            case SEND_INSULT -> {
+              String insultMessage = grammar.textGenerator("start", jsonReader.jsonProcess());
 
-        semaphore.release();
+            }
+            case CONNECT_MESSAGE -> {
+
+              socketMap.put(protocol.decode(type, line), socket);
+              // success
+              String reponse = "" + socketMap.size();
+              // fail
+
+              out.write(protocol.encode(MessageType.CONNECT_RESPONSE, reponse));
+            }
+            case QUERY_USERS -> {
+              String username = protocol.decode(type, line);
+              Socket socket1 = socketMap.get(username);
+
+              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+              out.write(protocol.encode(MessageType.QUERY_RESPONSE, username));
+            }
+            case DISCONNECT_MESSAGE -> {
+              String username = protocol.decode(type, line);
+              Socket socket1 = socketMap.get(username);
+              socket1.close();
+              socketMap.remove(username);
+            }
+          }
+
+          if (socketMap.size() == 0)
+            break;
+
+          in.close();
+
+          semaphore.release();
+        }
       } catch (IOException e) {
         e.printStackTrace();
       } catch (InterruptedException e) {
