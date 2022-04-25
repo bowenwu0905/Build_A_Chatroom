578a011b7a375a105a71f68cf9a4c2cac3c5e1db
zitao <Ofcourseistillloveyou>
2022-04-24 21:52:50 -0700

    update on test OutputHandler

--- assignment6/src/test/java/client/InputHandlerTest.java
+++ assignment6/src/test/java/client/InputHandlerTest.java
@@ -2,21 +2,172 @@ package client;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import protocol.MessageType;
+import protocol.Protocol;
+import protocol.ProtocolImp;
 
 class InputHandlerTest {
+  String userName;
+  String receiverName;
+  String text;
+  DataInputStream fromServer;
+  DataOutputStream toServer;
+
+  OutputHandler o1;
+  InputHandler i1;
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
+  List<String> allUsers = Arrays.asList("u1","u2","u3");
 
   @BeforeEach
-  void setUp() {
+  void setUp() throws FileNotFoundException {
+    userName = "u1";
+    receiverName = "r1";
+    text = "hello";
+    toServer = new DataOutputStream(new FileOutputStream("file.txt"));
+    fromServer = new DataInputStream(new FileInputStream("file.txt"));
+    o1 = new OutputHandler(userName,fromServer);
+    i1 = new InputHandler(userName,toServer);
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+  @AfterEach
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
+  }
+
+
+  @Test
+  void inputParse1() throws IOException {
+    i1.inputParse("logoff");
+    assertEquals(Protocol.messageToIdr.get(MessageType.DISCONNECT_MESSAGE),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(userName.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] senderUser = new byte[userName.length()];
+    fromServer.read(senderUser);
+    String senderName = new String(senderUser, StandardCharsets.UTF_8);
+    assertEquals(userName,senderName);
   }
 
   @Test
-  void inputParse() {
+  void inputParse2() throws IOException {
+    i1.inputParse("who");
+    assertEquals(Protocol.messageToIdr.get(MessageType.QUERY_USERS),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(userName.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] senderUser = new byte[userName.length()];
+    fromServer.read(senderUser);
+    String senderName = new String(senderUser, StandardCharsets.UTF_8);
+    assertEquals(userName,senderName);
   }
 
   @Test
-  void connectServer() {
+  void inputParse3() throws IOException {
+    i1.inputParse("@"+receiverName+" "+text);
+    assertEquals(Protocol.messageToIdr.get(MessageType.DIRECT_MESSAGE),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(userName.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] senderUser = new byte[userName.length()];
+    fromServer.read(senderUser);
+    String senderName = new String(senderUser, StandardCharsets.UTF_8);
+    assertEquals(userName,senderName);
+
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(receiverName.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] receiverUser = new byte[receiverName.length()];
+    fromServer.read(receiverUser);
+    String rName = new String(receiverUser, StandardCharsets.UTF_8);
+    assertEquals(receiverName,rName);
+
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(text.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] textByte = new byte[text.length()];
+    fromServer.read(textByte);
+    String m = new String(textByte, StandardCharsets.UTF_8);
+    assertEquals(text,m);
+  }
+
+  @Test
+  void inputParse4() throws IOException {
+    i1.inputParse("!"+receiverName);
+    assertEquals(Protocol.messageToIdr.get(MessageType.SEND_INSULT),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(userName.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] senderUser = new byte[userName.length()];
+    fromServer.read(senderUser);
+    String senderName = new String(senderUser, StandardCharsets.UTF_8);
+    assertEquals(userName,senderName);
+
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(receiverName.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] receiverUser = new byte[receiverName.length()];
+    fromServer.read(receiverUser);
+    String rName = new String(receiverUser, StandardCharsets.UTF_8);
+    assertEquals(receiverName,rName);
+
+  }
+
+  @Test
+  void inputParse5() throws IOException {
+    i1.inputParse("@all "+text);
+    assertEquals(Protocol.messageToIdr.get(MessageType.BROADCAST_MESSAGE),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(userName.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] senderUser = new byte[userName.length()];
+    fromServer.read(senderUser);
+    String senderName = new String(senderUser, StandardCharsets.UTF_8);
+    assertEquals(userName,senderName);
+
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(text.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] textByte = new byte[text.length()];
+    fromServer.read(textByte);
+    String m = new String(textByte, StandardCharsets.UTF_8);
+    assertEquals(text,m);
+
+  }
+
+
+
+  @Test
+  void connectServer() throws IOException {
+    i1.connectServer();
+    assertEquals(Protocol.messageToIdr.get(MessageType.CONNECT_MESSAGE),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    assertEquals(userName.length(),fromServer.readInt());
+    assertEquals(' ',fromServer.readChar());
+    byte[] senderUser = new byte[userName.length()];
+    fromServer.read(senderUser);
+    String senderName = new String(senderUser, StandardCharsets.UTF_8);
+    assertEquals(userName,senderName);
+
   }
 
   @Test
