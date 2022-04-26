498471c06adbe1f5168c5ca2960f23fbe1bed2a2
xcjiang <jiang.xiaoc@northeastern.edu>
2022-04-25 19:10:01 -0700

    a6 server code

--- assignment6/src/main/java/server/Server.java
+++ assignment6/src/main/java/server/Server.java
@@ -1,19 +1,12 @@
 package server;
 
-import client.Client;
-import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Semaphore;
-import protocol.Protocol;
 
 /**
  * server class
@@ -24,49 +17,107 @@ public class Server {
 
   private ServerSocket serverSocket;
   private Semaphore semaphore;
-  private Protocol protocol;
   private ConcurrentHashMap<String, Socket> socketMap;
 
   private ConcurrentHashMap<String, DataOutputStream> outMap;
 
   private static final int CLIENT_LIMIT = 10;
 
+  public Server() {
+  }
+
   public void start(int port) throws IOException {
-    serverSocket = new ServerSocket(port);
-    System.out.println("Server start to listen port " + port);
     semaphore = new Semaphore(CLIENT_LIMIT);
     socketMap = new ConcurrentHashMap<>(CLIENT_LIMIT);
     outMap = new ConcurrentHashMap<>(CLIENT_LIMIT);
+    serverSocket = new ServerSocket(port);
+    System.out.println("Server start to listen port " + port);
   }
 
-  public void run() throws IOException, InterruptedException {
+  public void run() throws IOException {
     // basically, we want to limit 10 clients, we need to set a thread pool that
     // only process 10 socket connection
-    while (true) {
-      this.semaphore.acquire();
-      Socket socket = this.serverSocket.accept();
-      new Thread(new ServerHandler(semaphore, socket, socketMap, outMap)).start();
-      if (this.semaphore.availablePermits() == CLIENT_LIMIT) {
-        System.out.println("enter q to close server or wait clients to connect");
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        if (input.equals("q")) {
-          break;
-        }
+    try {
+      while (true) {
+        this.semaphore.acquire();
+        Socket socket = this.serverSocket.accept();
+        new Thread(new ServerHandler(semaphore, socket, socketMap, outMap)).start();
       }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      serverSocket.close();
     }
   }
 
-  public void stopServer() throws IOException {
-    serverSocket.close();
-  }
-
-  public static void main(String[] args) throws IOException, InterruptedException {
+  public static void main(String[] args) throws IOException {
     Server server = new Server();
     // 需要从command输入么?
     server.start(10000);
     server.run();
-    server.stopServer();
   }
 
+  public ServerSocket getServerSocket() {
+    return serverSocket;
+  }
+
+  public void setServerSocket(ServerSocket serverSocket) {
+    this.serverSocket = serverSocket;
+  }
+
+  public Semaphore getSemaphore() {
+    return semaphore;
+  }
+
+  public void setSemaphore(Semaphore semaphore) {
+    this.semaphore = semaphore;
+  }
+
+  public ConcurrentHashMap<String, Socket> getSocketMap() {
+    return socketMap;
+  }
+
+  public void setSocketMap(
+      ConcurrentHashMap<String, Socket> socketMap) {
+    this.socketMap = socketMap;
+  }
+
+  public ConcurrentHashMap<String, DataOutputStream> getOutMap() {
+    return outMap;
+  }
+
+  public void setOutMap(
+      ConcurrentHashMap<String, DataOutputStream> outMap) {
+    this.outMap = outMap;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Server)) {
+      return false;
+    }
+    Server server = (Server) o;
+    return Objects.equals(getServerSocket(), server.getServerSocket())
+        && Objects.equals(getSemaphore(), server.getSemaphore())
+        && Objects.equals(getSocketMap(), server.getSocketMap())
+        && Objects.equals(getOutMap(), server.getOutMap());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getServerSocket(), getSemaphore(), getSocketMap(), getOutMap());
+  }
+
+  @Override
+  public String toString() {
+    return "Server{" +
+        "serverSocket=" + serverSocket +
+        ", semaphore=" + semaphore +
+        ", socketMap=" + socketMap +
+        ", outMap=" + outMap +
+        '}';
+  }
 }
